4c76044b9265d41ba0e1df0f0f4900d9acc6cfc5
ptatasu <garymason922@gmail.com>
2025-11-01 17:59:50 +0800

    Completed front page layout and design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import hero from './img/hero.jpg';
+import camera from './img/assets/camera_icon.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="App">
+        <span id='hero-title' >Flashy.Pixl</span>
+        <span id='hero-label'>Instant Fun.<br />Timeless<br />Snaps.</span>
+        <p id='app-label-1'>photobooth made by jamesphiliph</p>
+        <div className='button hero-button'><img src={camera} alt="" srcset=""/></div>
+        <p id='app-label-2'>Handcrafted pixels for my favorite person, angela.</p>
+        <img id='hero-image' src = {hero}/>
+        <div id="footer">
+          <a id="about" className='link' href='#'>about</a>
+          <a id="madeby" className='link' href='https://github.com/ptatasu'>made by jamesphiliph</a>
+          <p className='version'>Version 1.00.00</p>
+        </div>
+      </div>
     </div>
   );
 }
