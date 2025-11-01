0f3baffaaba40ca3308887bc32df4fb28cd0ad07
ptatasu <garymason922@gmail.com>
2025-11-01 23:12:28 +0800

    feat: add splash screen and refactor App.js
    Added a new splash screen for app startup and moved App.js logic into a separate component for better structure and maintainability.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
-import hero from './img/hero.jpg';
-import camera from './img/assets/camera_icon.svg';
+import Main from './Main';
 
 function App() {
   return (
-    <div className='container'>
-      <div className="App">
-        <span id='hero-title' >Flashy.Pixl</span>
-        <span id='hero-label'>Instant Fun.<br />Timeless<br />Snaps.</span>
-        <p id='app-label-1'>photobooth made by jamesphiliph</p>
-        <div className='button hero-button'><img src={camera} alt="" srcset=""/></div>
-        <p id='app-label-2'>Handcrafted pixels for my favorite person, angela.</p>
-        <img id='hero-image' src = {hero}/>
-        <div id="footer">
-          <a id="about" className='link' href='#'>about</a>
-          <a id="madeby" className='link' href='https://github.com/ptatasu'>made by jamesphiliph</a>
-          <p className='version'>Version 1.00.00</p>
-        </div>
-      </div>
-    </div>
+      <Main />
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,38 @@
+import './Main.css'
+import React, { useRef } from 'react';
+import hero from './img/assets/hero.jpg';
+import camera from './img/assets/camera_icon.svg';
+import logo from './img/assets/logo.png'
+
+function Main() {
+
+    const loadingRef = useRef(null);
+    const interval = setInterval(splashScreen, 2000);
+    function splashScreen() {
+        loadingRef.current.className= 'done-loading';
+        clearInterval(interval);
+    }
+
+  return (
+    <div className='container'>
+      <div id='loading' ref={loadingRef} >
+        <img id='logo' className='blink-1' src={logo} draggable='false' />
+      </div>
+      <div className="App">
+        <span id='hero-title' >Flashy.Pixl</span>
+        <span id='hero-label'>Instant Fun.<br />Timeless<br />Snaps.</span>
+        <p id='app-label-1'>photobooth made by jamesphiliph</p>
+        <div className='button hero-button'><img src={camera} alt="" srcset=""/></div>
+        <p id='app-label-2'>Handcrafted pixels for my favorite person, angela.</p>
+        <img id='hero-image' src = {hero}/>
+        <div id="footer">
+          <a id="about" className='link' href='#'>about</a>
+          <a id="madeby" className='link' href='https://github.com/ptatasu'>made by jamesphiliph</a>
+          <p className='version'>Version 1.00.1</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main;
