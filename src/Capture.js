8ccdad0f233b62e73ebc137796643a84271f7807
ptatasu <garymason922@gmail.com>
2025-11-08 22:08:13 +0800

    Add capture functionality and implement responsive page layout

--- src/Capture.js
+++ src/Capture.js
@@ -1,25 +1,46 @@
-import './Capture.css';
-import Webcam from 'react-webcam';
-import camera from './img/assets/camera_icon.svg';
+import "./Capture.css";
+import Webcam from "react-webcam";
+import camera from "./img/assets/camera_icon.svg";
+import { useCallback, useRef } from "react";
 
 function Capture() {
   const videoConstraints = {
-  width: 533,
-  height: 400,
-  facingMode: "user"
-};
+    width: 533,
+    height: 400,
+    facingMode: "user",
+  };
+
+  function CapturePhoto() {
+    const imageSrc = webcamRef.current.getScreenshot();
+    let base64 = JSON.stringify(imageSrc).slice(1, -1);
+    sda.current.src = base64;
+  }
+
+  const webcamRef = useRef(null);
+  const sda = useRef(null);
+
   return (
-    <div className='capture-container'>
-      <div id='capture-content'>
+    <div className="capture-container">
+      <div id="capture-content">
         <div id="webcam">
-          <Webcam audio={false} screenshotFormat="image/jpeg" videoConstraints={videoConstraints} />
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            mirrored={true}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
         </div>
         <div id="timer">
-          <p id='label'>Timer</p>
+          <p id="label">Timer</p>
           <div class="timer-wrapper">
-            <div className='textbox' >3s</div>
-            <div class="timer-button" id='increment'>+</div>
-            <div class="timer-button" id='decrement'>-</div>
+            <div className="textbox">3s</div>
+            <div class="timer-button" id="increment">
+              +
+            </div>
+            <div class="timer-button" id="decrement">
+              -
+            </div>
           </div>
         </div>
         <div id="bottom-label">
@@ -27,13 +48,18 @@ function Capture() {
           <p id="bottom-label-subtext">let us capture this moment</p>
         </div>
         <div id="capture-button">
-          <p id='label'>Capture</p>
-          <div className='hero-button' ><img src={camera}/></div>
-          <div className='textbox' id='image-count' >0/4</div>
+          <p id="label">Capture</p>
+          <div className="hero-button" onClick={CapturePhoto}>
+            <img src={camera} />
+          </div>
+          <div className="textbox" id="image-count">
+            0/4
+          </div>
         </div>
+        <img src={camera} ref={sda} /> {/*placeholder to see the image*/}
       </div>
     </div>
-  )
+  );
 }
 
 export default Capture;
